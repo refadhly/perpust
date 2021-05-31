76a359ab7c1b627cbfd57e711f90f18221c3ac07
refadhly <refadliy@gmail.com>
2021-05-31 14:32:06 +0700

    test3

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,11 +56,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'ec2-54-243-92-68.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'd170lamrg01hsu'),
+            'username' => env('DB_USERNAME', 'iszyzlvmczgecq'),
+            'password' => env('DB_PASSWORD', '897279b756c27d5ccee8a5cc084c66ba9772b8fab88daef79aed36805b5ceb94'),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
